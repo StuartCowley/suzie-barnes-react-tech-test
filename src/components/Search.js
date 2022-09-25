819bf05d0cd66c96d0a4aaff2558cfa513245813
Suzannah Barnes <smgarland@hotmail.co.uk>
2022-09-25 22:44:55 +0100

    submit button refreshes page

---
+++ src/components/Search.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "../styles/search.css";
+
+const Search = () => {
+    const [value, setValue] = useState();
+    return (
+        <>
+            <form>
+                <input
+                    className="search-input"
+                    type="text"
+                    onChange={(e) => setValue(e.target.value)}
+                />
+                <button className="search-button" type="submit">
+                    Submit
+                </button>
+            </form>
+        </>
+    );
+};
+
+export default Search;
